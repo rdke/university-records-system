0f59e37e42fd56f6750b83fb2ca32dac60e95fce
Sveta Kovakina <skovakina@adobe.com>
2026-09-19 12:44:25 -0700

    keep university_records name

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE', 'university'),
+        'NAME': os.environ.get('MYSQL_DATABASE', 'university_records'),
         'USER': os.environ.get('MYSQL_USER', 'root'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD', ''),
         'HOST': os.environ.get('MYSQL_HOST', '127.0.0.1'),
